f8b99d3cde637a1f85cb9eb32067e34971191a50
KeilerX <40313142+KeilerX@users.noreply.github.com>
2022-01-31 03:19:20 -0500

    fix

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = 'https://division-managment-challenge.herokuapp.com/'//'http://127.0.0.1:8000';
+const BACKEND_URL = 'https://division-managment-challenge.herokuapp.com'//'http://127.0.0.1:8000';
 
 export default BACKEND_URL;
